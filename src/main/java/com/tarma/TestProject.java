1cae28c9ecc78722f1413a2937a8687043a43a4b
Enrico Bottani <enrico.bottani@cryms.com>
2021-01-30 20:31:39 +0100

    What it was supposed to be the initial commit

---
+++ src/main/java/com/tarma/TestProject.java
@@ -0,0 +1,24 @@
+package com.tarma;
+
+import info.magnolia.module.ModuleLifecycle;
+import info.magnolia.module.ModuleLifecycleContext;
+
+/**
+ * This class is optional and represents the configuration for the TEST_PROJECT module.
+ * By exposing simple getter/setter/adder methods, this bean can be configured via content2bean
+ * using the properties and node from <tt>config:/modules/TEST_PROJECT</tt>.
+ * If you don't need this, simply remove the reference to this class in the module descriptor xml.
+ * See https://documentation.magnolia-cms.com/display/DOCS/Module+configuration for information about module configuration.
+ */
+public class TestProject implements ModuleLifecycle {
+    /* you can optionally implement info.magnolia.module.ModuleLifecycle */
+    @Override
+    public void start(ModuleLifecycleContext moduleLifecycleContext) {
+        System.out.println("Module started!");
+    }
+
+    @Override
+    public void stop(ModuleLifecycleContext moduleLifecycleContext) {
+
+    }
+}
